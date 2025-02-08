d32cb080d2d8b853689f4278a119e214da6b195b
Lucijoy <ljfloan2@students.unwsp.edu>
2025-02-07 20:07:37 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,3 +1,7 @@
+# Author: Lucia Floan
+# Date: Feb 7, 2025
+# Title: Age Classifier
+
 # Write a program that asks the user to enter a person's age.  The program should display a message indicating whether the person is an infant, a child, a teenager, or an adult.  Following are the guidelines:
 
 # If the person is 1 year old or less, it should display "infant" (without quotes).
@@ -6,14 +10,25 @@
 # If the person is at least 20 year old, it should display "adult".
 
 def categorize_age(age):
-    ageCategory = "TBD"
+    # Pseudocode:
+    # START
+    # 1. Prompt the user to enter a person's age.
+    # 2. IF age is less than or equal to 1, display "infant"
+    # 3. IF age is greater than 1 and less than 13, display "child"
+    # 4. IF age is greater than or equal to 13 and less than 20, display "teenager"
+    # 5. IF age is greater than or equal to 20, display "adult"
+    # END
+    
     ######################
-    # WRITE YOUR CODE HERE
+    if age <= 1: 
+        return 'infant'
+    if age > 1 and age < 13: 
+        return 'child'
+    if age >= 13 and age < 20: 
+        return 'teenager'
+    if age >= 20: 
+        return 'adult'
     ######################
-
-
-    return ageCategory
-
 
 #### This piece of the code has been done for you,
 #### you only need to worry about the actual shipping 
